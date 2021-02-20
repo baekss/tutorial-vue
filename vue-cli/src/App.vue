923c65d93a5e15bc50ba3d7c64913f966f5e6912
bss <baekss0409@gmail.com>
2021-02-20 23:59:53 +0900

    feat: Vue Cli props & event

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<!-- 컴포넌트 명명법(두 단어 이상으로 조합 권장) 종류 -->
+<!--
+<hello-world></hello-world>
+<HelloWorld></HelloWorld>
+<HelloWorld/>
+-->
+  <app-header v-bind:propsdata="str" v-on:renew="changeText"></app-header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
 
 export default {
-  name: 'App',
+// 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
+  data: function() {
+    return {
+      str: 'hi'
+    }
+  },
   components: {
-    HelloWorld
+    'app-header': AppHeader
+  },
+  methods: {
+    changeText: function() {
+      this.str = 'have a nice day';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
