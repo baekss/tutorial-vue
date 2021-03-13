f987f4610a07ee6f31e078c6c3cb0d86c61d28e0
bss <baekss0409@gmail.com>
2021-03-13 17:50:00 +0900

    refactor: Todo app 향상된 객체 리터럴

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -16,39 +16,39 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-  data: function() {
+  data() {
     return {
       todoItems: []
     }
   },
   methods: {
-    addOneItem: function(todoItem) {
-      var obj = {completed: false, item: todoItem};
+    addOneItem(todoItem) {
+      const obj = {completed: false, item: todoItem};
       localStorage.setItem(obj.item, JSON.stringify(obj));
       this.todoItems.push(obj);
     },
-    removeOneItem: function(todoItem, index) {
+    removeOneItem(todoItem, index) {
       localStorage.removeItem(todoItem.item);
       this.todoItems.splice(index, 1);
     },
-    toggleOneItem: function(todoItem, index) {
+    toggleOneItem(todoItem, index) {
       // 하위 컴포넌트에서 올려준 데이터(ref)를 통해 로직을 수행하는 접근법은 권장하지 않음
       // todoItem.completed = !todoItem.completed;
       this.todoItems[index].completed = !this.todoItems[index].completed;
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     },
-    clearAllItems: function() {
+    clearAllItems() {
       localStorage.clear();
       this.todoItems = [];
     }
   },
   // Vue 인스턴스 라이프사이클 Hook
-  created: function() {
+  created() {
     if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
+      for (let i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
-          var value = localStorage.getItem(localStorage.key(i));
+          const value = localStorage.getItem(localStorage.key(i));
           this.todoItems.push(JSON.parse(value));
         }
       }
@@ -56,10 +56,11 @@ export default {
   },
   components: {
     // 컴포넌트 태그명: 컴포넌트 내용
-    'TodoHeader': TodoHeader,
-    'TodoInput': TodoInput,
-    'TodoList': TodoList,
-    'TodoFooter': TodoFooter
+    // 컴포넌트 태그명, 속성명 동일시 다음과 같이 향상된 객체 리터럴로 표현
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
   }
 }
 </script>
